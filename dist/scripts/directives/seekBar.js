d276861a0620922ec1c19b34e9344af4e4307a1e
Kevin <kkornafel82@yahoo.com>
2016-03-19 12:05:53 -0700

    anguular checkpoint 10 revised in mentor meeting

--- dist/scripts/directives/seekBar.js
+++ dist/scripts/directives/seekBar.js
@@ -69,9 +69,7 @@
         };
 
          var notifyOnChange = function(newValue) {
-          if (typeof scope.onChange === 'function') {
-           scope.onChange({value: newValue});
-          }
+           scope.onChange({x: newValue});
          };
        }
      }
